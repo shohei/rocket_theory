689ed8d3730ed9bbb504da271ebf5d27f1204af2
Shohei Aoki <shoaok@gmail.com>
2022-03-29 17:41:20 +0300

    temporal commit

--- launch_trajectory.m
+++ launch_trajectory.m
@@ -26,23 +26,53 @@ l20= 1; %todo
 l30 = 1; %todo
 l40 = 1; %todo
 [t,xnext] = ode23s(@adjoint_ode, tspan, [x0,y0,vx0,vy0,l10,l20,l30,l40]);
-x = xnext(1);
-y = xnext(2);
-vx = xnext(3);
-vy = xnext(4);
-l1 = xnext(5);
-l2 = xnext(6);
-l3 = xnext(7);
-l4 = xnext(8);
+x_final = xnext(1);
+y_final = xnext(2);
+vx_final = xnext(3);
+vy_final = xnext(4);
+l1_final = xnext(5);
+l2_final = xnext(6);
+l3_final = xnext(7);
+l4_final = xnext(8);
 aTf = C/(tau_m-t(end));
-r = sqrt(x^2+y^2);
-E1 = r-rf;
-E2 = sqrt(vx^2+vy^2) - vf;
-E3 = x/r*vx+y/r*vy - vrf;
-E4 = l1*y - l2*x;
-cos_th = l3/sqrt(l3^2+l4^2);
-sin_th = l4/sqrt(l3^2+l4^2);
-E5 = 1+l1*vx+l2*vy+l3*(aTf*cos_th-mu/r^3*x)+l4*(aTf*sin_th-mu/r^3*y);
+r_final = sqrt(x_final^2+y_final^2);
+E1 = r_final-rf;
+E2 = sqrt(vx_final^2+vy_final^2) - vf;
+E3 = x_final/r_final*vx_final+y_final/r_final*vy_final - vrf;
+E4 = l1_final*y_final - l2_final*x_final;
+cos_th_final = l3_final/sqrt(l3_final^2+l4_final^2);
+sin_th_final = l4_final/sqrt(l3_final^2+l4_final^2);
+E5 = 1+l1_final*vx_final+...
+       l2_final*vy_final+...n
+       l3_final*(aTf*cos_th_final-mu/r_final^3*x_final)+...
+       l4_final*(aTf*sin_th_final-mu/r_final^3*y_final);
+% if max(E1,E2,E3,E4,E5) < 1e-2
+%   break;
+% end
+
+syms x y vx vy l1 l2 l3 l4 me aT;
+H = 1+...
+    l1*vx+...
+    l2*vy+...
+    l3*(aT*cos(atan2(y,x))-me/sqrt(x^2+y^2)^3*x)+...
+    l4*(aT*sin(atan2(y,x))-me/sqrt(x^2+y^2)^3*y);
+f1 = vx;
+f2 = vy;
+f3 = l3*(aT*cos(atan2(y,x))-me/sqrt(x^2+y^2)^3*x);
+f4 = l4*(aT*sin(atan2(y,x))-me/sqrt(x^2+y^2)^3*y);
+A1 = diff(f1, x);
+B1 = 0;
+C1 = diff(diff(H, x), x);
+A2 = diff(f2, y);
+B2 = 0;
+C2 = diff(diff(H, y), y);
+A3 = diff(f3, vx);
+B3 = 0;
+C3 = diff(diff(H, vx), vx);
+A4 = diff(f4, vy);
+B4 = 0;
+C4 = diff(diff(H, vy), vy);
+
 
 
 
